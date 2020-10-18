b8d14dc3d7e672783ca8e32e9a30d2739eb92110
tejaswanama111 <tejaswanama2006@gmail.com>
2020-10-18 09:57:10 -0700

    API

--- sketch.js
+++ sketch.js
@@ -8,10 +8,12 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var score = 0;
+
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +47,27 @@ function setup(){
 }
 
 function draw(){
+    if (backgroundImg){
     background(backgroundImg);
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("Score :" + score, width-300, 50);
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +96,18 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+ var response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London");
+ var responseJSON = await response.json();
+ var datetime = responseJSON.datetime;
+ var hour = datetime.slice(11,13);
+ if(hour>=06 && hour<=19) {
+     bg = "sprites/bg.png";
+ }
+ else {
+     bg = "sprites/bg2.jpg";
+ }
+ backgroundImg = loadImage(bg);
 }
